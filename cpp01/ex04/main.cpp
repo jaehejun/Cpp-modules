ead3516bd13c1f36edc9f3d48e282dccd82fa5ef
Jaehee Jung <wogml414@gmail.com>
2024-02-28 21:11:10 +0900

    getline(), std::cin, 마지막줄 개행없는데도 개행 붙여서 파일을 만드는 문제가 생김

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,44 +1,47 @@
-//file open -> 다읽고 find() 로 s1 찾음
-//s1 -> s2로 바꿈
+#include <fstream>
+#include <iostream>
 
-//반드시 open() 후 close()
-
-//getlin()쓰면 개행붙여줘야대서 std::cin으로 해야한다?(여민도치)
-
-//파일을 한글자씩 읽는 방법이 이씀 (서쏜)
-
-//s2로 바꿨는데, 또 바꾸는 실수가 날 수 있다.
-
-//aa -> a 로 바꾸고싶은데
-//aaaa -> aa -> a 이렇게 되면 틀린다 (바꾼 후에 포시션 수정해야함?)
-//pointer 밀듯이..?
-
-//실수를 처리하는 두가지 방법
-//부동 소수점 - floating point 붕붕 떠다닌다
-//고정 소수점 에 관한게 c02
-
-
-#include "Replace.hpp"
-
-int main(int ac, char **av)
+void replace(std::string filename, std::string s1, std::string s2)
 {
-	if (ac != 4)
-	{
-		std::cout << "Invalid arguments" << std::endl;
-		return -1;
-	}
+    std::ifstream originalFile(filename);
+    std::ofstream replaceFile((filename + ".replace"));
+    std::string line;
 
-	std::string filename, s1, s2;
+    if (!originalFile.is_open() || !replaceFile.is_open())
+    {
+        std::cerr << "Error: Could not open file." << std::endl;
+        return;
+    }
 
-	filename = av[1];
-	s1 = av[2];
-	s2 = av[3];
+    while (std::getline(originalFile, line))
+    {
+        std::string::size_type pos = 0;
+		// line.find(s1, pos) : line에서 s1을 찾고, 그 위치를 반환한다. 찾지 못하면 std::string::npos를 반환한다
+		// std::string::npos 는 std::string::size_type 의 특수한 값으로 size_type의 최대 가능 값인 -1과 같다.
+		// std::string::size_type이 양의 정수 타입이므로, -1은 이 타입의 최대 값으로 자동 변환된다.
+		// 문자열의 위치(std::string::size_type pos)는 항상 0 이상의 값이기에 -1로 위치를 찾지 못했음을 나타내는 것이 가능함
+        while ((pos = line.find(s1, pos)) != std::string::npos)
+        {
+			// pos 부터 s1.length() 개의 문자 제거(lenght()번째 문자까지)
+            line.erase(pos, s1.length());
+			// pos 위치에 s2 삽입
+            line.insert(pos, s2);
+			// pos 위치를 s2.lenght()만큼 더해서 뒤로 밀어줌
+            pos += s2.length();
+        }
+		// 새로 만들 파일에 line을 출력하고, getline()함수가 건너뛰는 개행문자까지 입력
+        replaceFile << line << '\n';
+    }
 
-	// open(filename);
-	// filename의 모든 s1을 s2로 대체
-	// filename.replace 라는 새 파일에 복사
+    originalFile.close();
+    replaceFile.close();
+}
 
-	
-
-	return 0;
+int main(int argc, char **argv)
+{
+    if (argc == 4)
+        replace(argv[1], argv[2], argv[3]);
+    else
+        std::cerr << "Error: Invalid arguments." << std::endl;
+    return 0;
 }
