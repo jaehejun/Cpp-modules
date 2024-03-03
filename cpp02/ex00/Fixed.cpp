5e53291db869da1c58d13a37b8e78be1170c682a
Jaehee Jung <wogml414@gmail.com>
2024-03-03 18:53:26 +0900

    Fixed

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -1,6 +1,36 @@
 #include "Fixed.hpp"
 
-class Fixed
+Fixed::static const int fractionalBits = 8;
+
+Fixed::Fixed() : fixedPointNumberValue(0)
 {
-	int num;
-};
+	std::cout << "Default constructor called" << std::endl;
+}
+
+copy_cons
+Fixed::Fixed(Fixed &fixed)
+{
+	std::cout << "Copy constructor called" << std::endl;
+}
+
+copy_assignmnet operator
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	
+	return (raw value of the fixed-point value);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	sets the raw value of the fixed-point number
+}
--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -4,5 +4,27 @@
 #include <iostream>
 #include <string>
 
+class Fixed
+{
+	private:
+		int fixedPointNumberValue;
+		static const int fractionalBits;
+
+	public:
+	/* default constructor that initialize fixed_point number value to 0
+	Copy constructor
+	copy assignment operator overload
+	destructor
+	int getRawBits(void) const; that returns the raw value of the fixed-point value
+	void setRawBits(int const raw); that sets the raw value of the fixed-point number*/
+		Fixed();
+		//copy-constructor;
+		Fixed(Fixed &fixed);
+		//copy assignment operator overload(=)
+		
+		~Fixed();
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+};
 
 #endif
